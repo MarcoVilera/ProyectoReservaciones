337f2beb15bf90a5714a779e6b8e62560b240fe0
MarcoVilera <marcovilarev123@gmail.com>
2023-09-11 23:40:31 -0400

    Modificado: Criterio de busqueda y  Opcional

--- classes/sorting/QuickSort.py
+++ classes/sorting/QuickSort.py
@@ -1,23 +1,33 @@
 #TODO Documentar Funcionamiento
-def partition(left, right, arr, order):
+def partition(left, right, arr, order,crit1,crit2):
     pivot, ptr = arr[right], left
     for i in range(left, right):
         if order=="asc":
-            if arr[i] <= pivot:
-                arr[i], arr[ptr] = arr[ptr], arr[i]
-                ptr += 1
+            if crit2=="":
+                if getattr(arr[i],crit1) <= getattr(pivot,crit1):
+                    arr[i], arr[ptr] = arr[ptr], arr[i]
+                    ptr += 1
+            else:
+              if (getattr(arr[i],crit1) <= getattr(pivot,crit1)) and (getattr(arr[i],crit2)<= getattr(pivot,crit2)):
+                    arr[i], arr[ptr] = arr[ptr], arr[i]
+                    ptr += 1  
         else:
-            if arr[i] >= pivot:
-                arr[i], arr[ptr] = arr[ptr], arr[i]
-                ptr += 1
+            if crit2=="":
+                if getattr(arr[i],crit1) >= getattr(pivot,crit1):
+                    arr[i], arr[ptr] = arr[ptr], arr[i]
+                    ptr += 1
+            else:
+              if (getattr(arr[i],crit1) >= getattr(pivot,crit1)) and (getattr(arr[i],crit2)<= getattr(pivot,crit2)):
+                    arr[i], arr[ptr] = arr[ptr], arr[i]
+                    ptr += 1
     arr[ptr], arr[right] = arr[right], arr[ptr]
     return ptr
 
-def quicksort(left, right, arr, order):
+def quicksort(left, right, arr, order,crit1,crit2=""):
     if len(arr) == 1: # Terminating Condition for recursion. VERY IMPORTANT!
         return arr
     if left < right:
-        pi = partition(left, right, arr, order)
-        quicksort(left, pi - 1, arr,order) # Recursively sorting the left values
-        quicksort(pi + 1, right, arr,order) # Recursively sorting the right values
+        pi = partition(left, right, arr, order,crit1,crit2)
+        quicksort(left, pi - 1, arr,order,crit1,crit2) # Recursively sorting the left values
+        quicksort(pi + 1, right, arr,order,crit1,crit2) # Recursively sorting the right values
     return arr
